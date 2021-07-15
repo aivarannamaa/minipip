e42d0d5cc2028bf090b97a5b3bb898cbce4ff7b4
Aivar Annamaa <aivar.annamaa@gmail.com>
2021-07-15 08:28:32 +0300

    Start providing overrides

---
+++ overrides/Adafruit-Blinka/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="Adafruit-Blinka",
+    version="6.11.0",
+    description="Unpublished dummy package for installing CircuitPython libraries without real Blinka.",
+    long_description="Used by minipip when installing CircuitPython libraries from PyPI to a CircuitPython device",
+    author="Aivar Annamaa",
+    url="https://github.com/aivarannamaa/minipip",
+    install_requires=[],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: Implementation :: MicroPython",
+    ],
+)
